49c758af7bb6266aafa88d4fb8ea0654a3c37d6a
ANDRES <ANDRES@LAPTOP-K267TVE7>
2020-02-13 12:15:07 -0300

    commmit no.2 ( linea hola github)

--- src/hola/HolaMundo.java
+++ src/hola/HolaMundo.java
@@ -3,7 +3,7 @@ package hola;
 public class HolaMundo {
 
 	public static void main(String[] args) {		
-		System.out.println("Hola Mundo");
+		
 		System.out.println("Y Hola GitHUb");
 	}
 
---
+++ src/hola/holagit.java
@@ -0,0 +1,13 @@
+package hola;
+
+public class holagit {
+
+
+		public static void main(String[] args) {		
+			
+			System.out.println("Y Hola GitHUb");
+		}
+
+	}
+
+
